54aa3e64f1567f3f0fca9cc09709b395be29c242
Ishi <162271161+ishi-6@users.noreply.github.com>
2024-09-16 19:54:28 -0400

    env

--- act1/app/page.tsx
+++ act1/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         console.log(process.env.NEXT_PUBLIC_API_URL);
         //http://localhost:5000
         //${process.env.NEXT_PUBLIC_API_URL}
-        const res = await fetch(`http://localhost:5000/api/comics/panel01`);
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/comics/panel01`);
         const data = await res.json();
         setPanel01Url(data.imagePath); 
       } catch (err) {
