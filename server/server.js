5312636007c1373c56ce1b5f5d906f045e5f629c
Ishi <162271161+ishi-6@users.noreply.github.com>
2024-09-17 20:55:25 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => res.type('html').send(html));
 
 // middleware
 app.use(cors()); // express
@@ -60,39 +61,6 @@ const cloudinary = require('cloudinary').v2;
         api_secret: process.env.CLOUDINARY_KEY
     });
     
-
-    // cloudinary demo stuff
-    
-    // // Upload an image
-    //  const uploadResult = await cloudinary.uploader
-    //    .upload(
-    //        'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
-    //            public_id: 'shoes',
-    //        }
-    //    )
-    //    .catch((error) => {
-    //        console.log(error);
-    //    });
-    
-    // console.log(uploadResult);
-    
-    // // Optimize delivery by resizing and applying auto-format and auto-quality
-    // const optimizeUrl = cloudinary.url('shoes', {
-    //     fetch_format: 'auto',
-    //     quality: 'auto'
-    // });
-    
-    // console.log(optimizeUrl);
-    
-    // // Transform the image: auto-crop to square aspect_ratio
-    // const autoCropUrl = cloudinary.url('shoes', {
-    //     crop: 'auto',
-    //     gravity: 'auto',
-    //     width: 500,
-    //     height: 500,
-    // });
-    
-    // console.log(autoCropUrl);    
 })();
 
 
